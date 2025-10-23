f20e2e2db43e9268a32d8151c7bab25d16e06cc6
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2025-10-22 21:59:48 -0300

    alterações no veiculo service

--- src/service/VeiculoService.java
+++ src/service/VeiculoService.java
@@ -10,7 +10,12 @@ public class VeiculoService {
         VeiculoDAO veiculoDAO = new VeiculoDAO();
         veiculoDAO.Create(objeto);
     }
-
+    
+      public static List<Veiculo> Carregar() {
+        VeiculoDAO veiculoDAO = new VeiculoDAO();
+        return veiculoDAO.Retrieve();
+    }
+      
     public static Veiculo Carregar(int id) {
         VeiculoDAO veiculoDAO = new VeiculoDAO();
         return veiculoDAO.Retrieve(id);
